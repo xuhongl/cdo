045befb97d5e22b9e58cd087e26e910a6f61247f
Xuhong Liu <xuhliu@microsoft.com>
2023-09-28 12:09:32 -0400

    test2

--- Import-Guests.ps1
+++ Import-Guests.ps1
@@ -2,8 +2,8 @@ $GroupName = "Students23Fall"
 $TenantID = "632dab3f-61fd-4748-b017-76fb265a3bc3"
 $ClientId = "1b14c59e-ceaa-4d81-864d-f3dcbb8b0393"
 $SPNsecret = "CCY8Q~qm7dVbrMwSIgzbb1dWIVklxEl-MuflLdnt"
-$SemseterStudentList = "23Fstudent.xls"
-$List = "23Fstudent.xls"
+$SemseterStudentList = "csv1.csv"
+$List = "csv1.csv"
 
 $ClientSecretPass = ConvertTo-SecureString -String $SPNsecret -AsPlainText -Force
 
@@ -18,7 +18,7 @@ $UserList = Import-Csv -Path $List
 foreach ($user in $UserList){
     $Displayname = $User.FirstName+" "+$User.LastName
 
-    $user.email
+    $user.Email = $user.email
 
     New-MgInvitation -InvitedUserDisplayName $Displayname -InvitedUserEmailAddress $user.Email -SendInvitationMessage:$false -InviteRedirectUrl "https://algonquincdo.ca/Azure" 
 
